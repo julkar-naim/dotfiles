e50f20653d35271b09053cdb61e6b394025960e5
Julkar Naim <julkar.naim@abbvie.com>
2024-06-03 00:16:17 +0600

    lazyvim-config

---
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("i", "<C-BS>", "<C-w>", { noremap = true, silent = true, desc = "Ctrl backspace to delete word" })
+
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Toggle debugger breakpoint" })
+
+map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
---
+++ .config/lazyvim/lua/plugins/custom.lua
@@ -0,0 +1,53 @@
+local plugins = {
+  {
+    "voldikss/vim-floaterm",
+    keys = {
+      {
+        "<leader>r",
+        "<cmd>FloatermNew g++ -Wall -Wextra -Wshadow -fsanitize=undefined -std=c++17 % && ./a.out && read <cr>",
+        desc = "Rum C++ file",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        "codelldb",
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
+
+return plugins
